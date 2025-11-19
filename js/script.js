c6b65e306c8cdf8c4e2961efdde0b9c52dc7d00e
Gabiru <gbrlferz@gmail.com>
2025-11-19 05:20:42 -0300

    Improve js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const headerPlaceholder = document.getElementById("header-placeholder");
-  const footerPlaceholder = document.getElementById("footer-placeholder");
+document.addEventListener("DOMContentLoaded", async function() {
+  setupGallery();
+  await loadHeader();
+  await loadFooter();
+});
 
+function setupGallery() {
   const mainImage = document.getElementById("main-image");
   const thumbnails = document.querySelectorAll(".thumbnail");
+  let activeThumbnail;
 
   if (thumbnails.length > 0) {
-    thumbnails[0].classList.add("active-thumb");
+    activeThumbnail = thumbnails[0];
+    activeThumbnail.classList.add("active-thumb");
   };
 
   thumbnails.forEach(thumbnail => {
     thumbnail.addEventListener("click", function() {
-      thumbnails.forEach(thumbnail => {
-        thumbnail.classList.remove("active-thumb");
-      });
-      thumbnail.classList.add("active-thumb");
+      activeThumbnail.classList.remove("active-thumb");
+      activeThumbnail = this
+      activeThumbnail.classList.add("active-thumb");
       mainImage.src = thumbnail.src;
     });
   });
+}
 
-  fetch("/nav.html")
-    .then(response => response.text())
-    .then(data => {
-      headerPlaceholder.innerHTML = data;
+async function loadHeader() {
+  const headerPlaceholder = document.getElementById("header-placeholder");
+  try {
+    const navResponse = await fetch("/nav.html");
+    const navData = await navResponse.text();
+    headerPlaceholder.innerHTML = navData;
+  } catch (error) {
+    console.error("Could not load nav:", error);
+  }
 
-      const currentPage = window.location.pathname.split("/").pop();
-      const navLinks = headerPlaceholder.querySelectorAll("nav a");
+  const currentPage = window.location.pathname.split("/").pop();
+  const navLinks = headerPlaceholder.querySelectorAll("nav a");
+  navLinks.forEach(link => {
+    const linkPage = link.getAttribute("href").split("/").pop();
 
-      navLinks.forEach(link => {
-        const linkPage = link.getAttribute("href").split("/").pop();
+    if (linkPage === currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
 
-        if (linkPage === currentPage) {
-          link.classList.add("active");
-        }
-      });
-    });
-
-  fetch("/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      footerPlaceholder.innerHTML = data;
-    });
-});
+async function loadFooter() {
+  const footerPlaceholder = document.getElementById("footer-placeholder");
+  try {
+    const footerResponse = await fetch("/footer.html");
+    const footerData = await footerResponse.text();
+    footerPlaceholder.innerHTML = footerData;
+  } catch (error) {
+    console.error("Could not load footer:", error);
+  }
+}
